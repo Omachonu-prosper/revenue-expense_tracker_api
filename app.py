e1fb87dc43c11e6fd5e0768bd80b341f35b00488
Omachonu Prosper <omachonucodes@gmail.com>
2023-07-15 16:13:01 +0100

    Create api endpoint to download reports

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from datetime import datetime
+from openpyxl import Workbook
+import tempfile
+import os
 
 app = Flask(__name__)
 
@@ -83,6 +86,50 @@ def view_report(capture_type):
 		'status': True
 	}
 	return jsonify(response)
+
+@app.route('/download/<string:capture_type>/report')
+def download_report(capture_type):
+	"""Api endpoint to download report
+	"""
+	if capture_type.lower() not in ['expenses', 'revenue']:
+		return 'Not Found', 404
+
+	start_date = request.args.get('start_date')
+	end_date = request.args.get('end_date')
+	try:
+		start_date = datetime.strptime(start_date, '%Y-%m-%d')
+		end_date = datetime.strptime(end_date, '%Y-%m-%d')
+	except:
+		return 'Bad payload: Invalid start_date or end_date format', 400
+
+	# Create an excel file to write to
+	wb = Workbook()
+	sheet = wb.active
+	heading = ['S/N', 'Item', 'Amount', 'Date', 'Type']
+	s_n = 1
+	sheet.append(heading)
+
+	for obj in collection:
+		if obj['type'] == capture_type.lower():
+			obj_date = datetime.strptime(obj['date'], '%Y-%m-%d')
+			if start_date <= obj_date and end_date >= obj_date :
+				row = [
+					s_n,
+					obj['category'],
+					obj['amount'],
+					obj['date'],
+					obj['type']
+				]
+				sheet.append(row)
+				s_n += 1
+
+	# Save excel file temporarily on the server
+	with tempfile.NamedTemporaryFile(delete=False) as tmp:
+		filename = tmp.name
+		wb.save(filename)
+
+	# Return file to user for downloading
+	return send_file(filename, as_attachment=True, download_name=f"{capture_type}_data.xlsx")
 
 
 @app.route('/')
